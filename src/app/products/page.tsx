7a946c8689f9e6d1a2f9551eb0938e8145eb5b16
Saurav Dhande <dhandesaurav37@gmail.com>
2025-07-19 16:49:54 +0000

    add 4 products in a row in shop page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -187,7 +187,7 @@ export default function ProductsPage() {
         </div>
       </div>
 
-      <main className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 justify-items-center">
+      <main className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8 justify-items-center">
         {filteredProducts.length > 0 ? (
           <>
             {filteredProducts.map((product) => (
