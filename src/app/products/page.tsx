cfbf6143c608777933085eecd5f309ab3328778a
Saurav Dhande <dhandesaurav37@gmail.com>
2025-07-19 16:44:25 +0000

    in shop page increase the size of products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -190,7 +190,7 @@ export default function ProductsPage() {
       <main className="mt-12">
         {filteredProducts.length > 0 ? (
           <div className="flex justify-center">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
               {filteredProducts.map((product) => (
                 <ProductCard key={product.id} product={product} />
               ))}
